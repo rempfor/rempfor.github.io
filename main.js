0b516c822e025b7563cfbb00438fd8ea4b4852eb
rempfor <79374981+rempfor@users.noreply.github.com>
2022-03-08 15:48:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,18 @@ const audioBackspace = new Audio("k2backspace.mp3");
 
 const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+/**
+ * Блок повторного набора текста на клаве.
+ */
 audio.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
 
+/**
+ * 1 символ раз в 90мс. Затем его посимвольное удаление.
+ * Ниже текст, который будет выводиться на экран.
+ */
 async function writeText(content) {
    const letters = new Array();
    audio.play();
@@ -37,6 +44,10 @@ async function writeText(content) {
     await sleep(150);
 }
 
+/**
+ * Этот блок отвечает за реверс фона и текста. Если текст белый, то фон темным - и наоборот.
+ * @returns Promise, который завершается спустя 650мс (0.65с) после вызова.
+ */
 async function reverse() {
     return new Promise(res => {
         main.style.backgroundColor == "rgb(25, 25, 25)" ? main.style.backgroundColor = "white" : main.style.backgroundColor = "rgb(25, 25, 25)";
@@ -50,6 +61,9 @@ const startButton = document.getElementById("start");
 const panelTop = document.getElementById("overlay-top");
 const panelBottom = document.getElementById("overlay-bottom");
 
+/**
+ * Начало.
+ */
  async function startExecution() {
     startButton.style.opacity = "0";
     await sleep(1200);
@@ -64,6 +78,9 @@ const panelBottom = document.getElementById("overlay-bottom");
     })
 }
 
+/**
+ * Магия.
+ */
 async function startTextWriter() {
     await writeText("Привет.");
     await writeText("Собственно, это моё первое знакомство с Git...");
